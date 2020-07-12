1eff560654fa962856f763dda78b0b8e30e38c85
Matheus <matheusalexandre.sena99@gmail.com>
2020-07-12 08:16:11 -0300

    Movendo a camera com mousemove

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,28 +31,39 @@ const init = async (width, height) => {
   render.autoClear = true;
   document.body.appendChild(render.domElement);
 
-  // controls.addEventListener('change', () => render.render(scene, camera));
-
   controls.enableDamping = true;
   controls.dampingFactor = 0.05;
-
-  controls.screenSpacePanning = false;
-
   controls.minDistance = 1;
-  controls.maxDistance = 50;
+  controls.maxDistance = 5;
 
-  controls.maxPolarAngle = Math.PI / 2;
+  document.addEventListener('mousemove', _.debounce((e) => {
+    const vector = camera.position.clone();
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
+    if (e.screenX > width / 2) {
+      vector.x += 0.01;
+    } else {
+      vector.x -= 0.01;
+    }
+
+    if (e.screenY > height / 2) {
+      vector.y += 0.01;
+    } else {
+      vector.y -= 0.01;
+    }
+
+    camera.position.set(vector.x, vector.y, vector.z);
+  }, 5));
+
+  const geometry = new THREE.BoxGeometry(0.25, 0.25, 0.25);
   const material = new THREE.MeshLambertMaterial({ color: config.hexColors.hexPurple });
 
   const cubes = [];
 
-  for (let i = 0; i < 20; i += 1) {
+  for (let i = 0; i < 200; i += 1) {
     const c = new THREE.Mesh(geometry, material);
     c.position.x = (Math.random() - 0.5) * 10;
-    c.position.y = (Math.random() - 0.5) * 7;
-    c.position.z = (Math.random() - 0.5) * 5;
+    c.position.y = (Math.random() - 0.5) * 10;
+    c.position.z = (Math.random() - 0.5) * 10;
 
     const factor = (Math.random() >= 0.5) ? 1 : -1;
     cubes.push({ el: c, factor });
@@ -84,18 +95,10 @@ const init = async (width, height) => {
   animate();
 };
 
-const debounce = (func, wait) => {
-  let timer = null;
-  return () => {
-    clearTimeout(timer);
-    timer = setTimeout(func, wait);
-  };
-};
-
-window.addEventListener('load', debounce(() => {
+window.addEventListener('load', _.debounce(() => {
   init(window.innerWidth, window.innerHeight);
 }, 500));
 
-window.addEventListener('resize', debounce(async () => {
+window.addEventListener('resize', _.debounce(async () => {
   init(window.innerWidth, window.innerHeight);
 }, 500));
